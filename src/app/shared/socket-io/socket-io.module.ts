2c0c1a5674145d348921e0dbbfbd474b9cfc2772
Chris Kinzel <christopher.kinzel@ucalgary.ca>
2019-03-09 19:54:42 -0700

    - Fixed a bug where environment variables were missing in production (socketIOURL and userCookieKey) - Fixed a bug where production build would not load socketio module correctly due to AOT optimizations trying to instantiate socket.io at compile time

--- src/app/shared/socket-io/socket-io.module.ts
+++ src/app/shared/socket-io/socket-io.module.ts
@@ -1,15 +1,28 @@
 import {InjectionToken, ModuleWithProviders, NgModule} from '@angular/core';
 import * as socketio from 'socket.io-client';
 
-export const SocketIO = new InjectionToken<SocketIOClient.Socket>('SocketIO');
 export type SocketIOSocket = SocketIOClient.Socket;
+
+export const SOCKET_IO_URI = new InjectionToken<string>('__SOCKET_IO_URI__');
+export const SocketIO = new InjectionToken<SocketIOSocket>('SocketIO');
+
+export function SocketIOFactory(uri: string) {
+  return socketio(uri);
+}
 
 @NgModule()
 export class SocketIOModule {
   public static usingURI(uri: string): ModuleWithProviders {
     return {
       ngModule: SocketIOModule,
-      providers: [{provide: SocketIO, useValue: socketio(uri)}]
+      providers: [
+        {provide: SOCKET_IO_URI, useValue: uri},
+        {
+          provide: SocketIO,
+          useFactory: SocketIOFactory,
+          deps: [SOCKET_IO_URI]
+        }
+      ]
     };
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  socketIOURL: undefined, // Set automatically in production
+  userCookieKey: 'user'
 };
